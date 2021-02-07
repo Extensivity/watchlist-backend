42accf62ce745bb3f6b9854142fa9181c5e9390b
Ian Bell <IanBell702@gmail.com>
2021-02-07 14:38:21 -0800

    Added models to 'api.shows'

--- api/shows/models.py
+++ api/shows/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class Show(models.Model):
+  name = models.CharField(max_length=100)
+
+  def __str__(self):
+    return self.name
+
+class Season(models.Model):
+  name = models.CharField(max_length=100)
+  show = models.ForeignKey(Show, on_delete=models.CASCADE, related_name='seasons')
+  order = models.SmallIntegerField()
+
+  def __str__(self):
+    return self.name
+
+class Episode(models.Model):
+  name = models.CharField(max_length=100)
+  season = models.ForeignKey(Season, on_delete=models.CASCADE, related_name='episodes')
+  order = models.PositiveSmallIntegerField()
+  seen = models.BooleanField()
+
+  @property
+  def show(self):
+    return self.season.show
+
+  def __str__(self):
+    return self.name
